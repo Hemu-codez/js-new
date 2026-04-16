02cca78f8d7220123da1a0de93e125cda40e94e8
Hemu-codez <hemulogs@gmail.com>
2026-04-16 22:03:13 +0000

    Variables

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,13 @@
+const accountId = 122331
+let accountEmail = "hemu@gmail.com"
+var accountPassword = "12345678"
+accountCity = "Delhi"
+let accountStates;
+
+accountEmail = "hemi@google.com"
+accountPassword = "87654321"
+accountCity = "Jaipur"
+
+console.log(accountId)
+
+console.table([accountEmail, accountPassword, accountCity, accountStates, accountId])
