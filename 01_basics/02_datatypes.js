ffde44613127eef1350fb4c8623f79f13c269b1c
Hemu-codez <hemulogs@gmail.com>
2026-04-19 19:56:42 +0000

    Data Types

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,9 @@
+ let name = `Hemu`
+ let age = 21
+ let isLoggedIn = false
+ let email; //undefined
+ let phone = null //null
+
+ console.table([name, age, isLoggedIn, email ])
+ console.log(typeof name)
+ console.log(typeof null) //null is an object
